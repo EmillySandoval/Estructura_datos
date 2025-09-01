a51485cef901c91501013b123280e13d76c793ca
EmillySandoval <emily01082006@gmail.com>
2025-08-31 20:41:37 -0700

    array
    primer commit

---
+++ Array Multilenguaje/array.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int[] numeros = { 10, 20, 30, 40, 50 };
+        foreach (int n in numeros)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
